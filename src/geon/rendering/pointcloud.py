0b6b331831088649f923d8200e86bc055b41859c
Hristo <hristo.vassilev95@gmail.com>
2025-12-07 17:13:09 +0100

    before app refactor

--- src/geon/rendering/pointcloud.py
+++ src/geon/rendering/pointcloud.py
@@ -25,6 +25,15 @@ class PointCloudLayer(BaseLayer[PointCloudData]):
         self._active_field_name:    Optional[str] = None
         self._visibility_mask:      Optional[NDArray[np.bool_]] = None
 
+        self._mapper_fine:      Optional[vtk.vtkMapper] = None
+        self._mapper_coarse:    Optional[vtk.vtkMapper] = None
+
+        self._main_actor: Optional[vtk.vtkLODActor] = None
+
+        
+
+
+
         # index of currently displayed scalar in each vector field
         self._vf_active_index: dict[str, int] = {}
         
@@ -51,9 +60,10 @@ class PointCloudLayer(BaseLayer[PointCloudData]):
         self._visibility_mask = None
 
     def _build_pipeline(
-        self, 
-        renderer: vtk.vtkRenderer, 
+        self,
+        renderer: vtk.vtkRenderer,
         out_actors: list[vtk.vtkProp],
+        
         coarse_ratio: float = 0.01 # point ratio of coarse LOD
     ) -> None:
         
@@ -78,15 +88,20 @@ class PointCloudLayer(BaseLayer[PointCloudData]):
             return poly, mapper
         
         # build mappers
-        self._poly, mapper_fine = _build_points(points_np)
-        self._poly_coarse, mapper_coarse = _build_points(points_np[::int(1/coarse_ratio)])
+        self._poly, self._mapper_fine = _build_points(points_np)
+        self._poly_coarse, self._mapper_coarse = _build_points(points_np[::int(1/coarse_ratio)])
+
+        
 
         # actor
         actor = vtk.vtkLODActor()
-        actor.SetMapper(mapper_fine)
-        actor.AddLODMapper(mapper_coarse)
+        actor.SetMapper(self._mapper_fine)
+        actor.AddLODMapper(self._mapper_coarse)
 
+        
         out_actors.append(actor)
+        self._main_actor = actor
+
         self._init_visibility_mask()
 
         # further methods from old code:
@@ -104,6 +119,9 @@ class PointCloudLayer(BaseLayer[PointCloudData]):
     def update(self) -> None:
         ctf = None
 
+        if self._poly is None or self._mapper_fine is None:
+            return
+
         # get displayed field
         if self._active_field_name is not None:
             field = self.data.get_fields(self._active_field_name)[0]
@@ -112,26 +130,41 @@ class PointCloudLayer(BaseLayer[PointCloudData]):
         else:
             field = None
         
-        # get colors
-        if field is not None:
+        colors_np : Optional[NDArray[np.uint8]] = None # (N,3) colors
+        scalars_np : Optional[NDArray[np.float32]] = None # (N,) scalar
+
+        
+        # get colors/scalars for active field
+        # TODO: implement for coarse_mapper without code duplication
+        if field is None:
+            self._mapper_fine.ScalarVisibilityOff()
+            if self._main_actor is not None:
+                r,g,b, = theme.DEFAULT_OBJ_COLOR
+                self._main_actor.GetProperty().SetColor(r,g,b)
+                
+
+        else:
             
             data_visible = field.data[self._visibility_mask] \
                 if self._visibility_mask is not None else field.data
             
             if field.field_type == FieldType.COLOR:
-                if np.all((0. <= data_visible) & (data_visible <= 1.)):
-                    colors_np = np.astype(data_visible*255, np.uint8)
-                elif np.all((0 <= data_visible) & (data_visible <= 255)):
-                    colors_np = data_visible
+                if data_visible.ndim != 2:
+                    raise ValueError(f"Unexpected color field shape: {data_visible.shape}")
+                if np.all(np.logical_and(0. <= data_visible, data_visible <= 1.)):
+                    colors_np = (data_visible * 255).astype(np.uint8)
+                elif np.all(np.logical_and(0 <= data_visible, data_visible <= 255)):
+                    colors_np = data_visible.astype(np.uint8)
             
             elif field.field_type == FieldType.SCALAR:
-                colors_np = data_visible
+                scalars_np = np.asarray(data_visible, dtype=np.float32).reshape(-1)
                 color_map = field.color_map or ColorMap('default')
                 ctf = build_vtk_color_transfer_function(color_map)
             
             elif field.field_type == FieldType.VECTOR:
                 ind = self.vf_active_index[field.name]
-                colors_np = data_visible[:,ind]
+                ind = max(0, min(ind, data_visible.shape[1] - 1))
+                scalars_np = data_visible[:,ind]
                 color_map = field.color_map or ColorMap('default')
                 ctf = build_vtk_color_transfer_function(color_map)
 
@@ -147,6 +180,7 @@ class PointCloudLayer(BaseLayer[PointCloudData]):
                 if self._visibility_mask is not None:
                     colors_np = colors_np[self._visibility_mask] 
 
+        # geometry
         if self._visibility_mask is not None:
             visible_inds = np.nonzero(self._visibility_mask)[0]
             visible_points_np = self.data.points[visible_inds]
@@ -154,6 +188,30 @@ class PointCloudLayer(BaseLayer[PointCloudData]):
         else:
             visible_points_np = self.data.points
 
+        vtk_points = self._poly.GetPoints()
+        vtk_points.SetData(ns.numpy_to_vtk(visible_points_np, deep=False))
+        
+        # push scalars/colors to vtk
+        if colors_np is not None:
+            vtk_colors = ns.numpy_to_vtk(colors_np, deep=False)
+            vtk_colors.SetNumberOfComponents(3)
+            self._poly.GetPointData().SetScalars(vtk_colors)
+
+            self._mapper_fine.SetScalarModeToUsePointData()
+            self._mapper_fine.ScalarVisibilityOn()
+            self._mapper_fine.SetLookupTable(None)
+        elif scalars_np is not None:
+            vtk_scalars = ns.numpy_to_vtk(scalars_np, deep=False)
+            vtk_scalars.SetNumberOfComponents(1)
+            
+            self._mapper_fine.SetScalarModeToUsePointData()
+            self._mapper_fine.ScalarVisibilityOn()
+            if ctf is not None:
+                self._mapper_fine.SetLookupTable(ctf)
+                self._mapper_fine.SetUseLookupTableScalarRange(True)
+        else:
+            self._mapper_fine.ScalarVisibilityOff()
+        self._poly.Modified()
 
 
 
